8f0dae289f6e89bdcae1070d0e787c29ac45dcc9
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2018-05-23 22:12:08 -0300

    0.1

--- Cartas.js
+++ Cartas.js
@@ -1,12 +1,21 @@
-Berk_tropa = {
-    nome: 'Berk',
+Bherk_tropa = {
+    nome: 'Bherk',
     raca: 'Anão',
     classe: 'Clerigo',
+    id: 'Bherk',
+
+    corpo_a_corpo: true,
+    pesadas: true,
+    longo_alcance: true,
+    armadadura: true,
+
+    pvInical: 150,
     pontos_de_vida: 150,
     ataque: 70,
     defesa: 80,
     agilidade: 06,
     brutalidade: 13,
+    //html: "<img id="BherInimigo" onclick="Acao(Bherk_tropa_Inimigo)"class="cartas" src="Tropas/Bherk.jpg">"
     Efeito: function(){
 
     }
@@ -16,6 +25,14 @@ OrfaFloresta_tropa = {
     nome: 'Orfã da Floresta',
     raca: 'Humana/Animal',
     classe: 'Druida/Fera',
+    id: 'OrfaFloresta',
+
+    corpo_a_corpo: false,
+    pesadas: false,
+    longo_alcance: false,
+    armadadura: false,
+
+    pvInical: 100,
     pontos_de_vida: 100,
     ataque: 20,
     defesa: 30,
@@ -33,6 +50,14 @@ TrollSafira_tropa = {
     nome: 'Troll de Safira',
     raca: 'Troll',
     classe: 'Selvagem',
+    id: 'TrollSafira',
+
+    corpo_a_corpo: false,
+    pesadas: true,
+    longo_alcance: false,
+    armadadura: false,
+
+    pvInical: 120,
     pontos_de_vida: 120,
     ataque: 85,
     defesa: 90,
@@ -56,7 +81,7 @@ ArmaduraNobre_arma = {
 
 LançaDruida_arma = {
     nome: 'Lança Druida',
-    tipo: 'Agilidade(' + 4 + ')Brutalidade(' + 4,
+    tipo: 'Agilidade(' + 4 + ') Brutalidade(' + 4 + ')',
     modAtaque: +10,
     modDefesa: +00,
     Efeito: function(){
@@ -68,3 +93,23 @@ LançaDruida_arma = {
         }
     }
 }
+
+
+//                                                    <img id="BherInimigo" onclick="Acao(Bherk_tropa_Inimigo)"class="cartas" src="Tropas/Bherk.jpg">
+
+
+let Bherk_tropa_Inimigo = {...Bherk_tropa};
+Bherk_tropa_Inimigo.id = "BherInimigo";
+
+/* for(kei in Bherk_tropa){
+    Bherk_tropa_Inimigo[key] = Bherk_tropa[key];
+}*/
+
+let TrollSafira_tropa_Inimigo = {...TrollSafira_tropa};
+TrollSafira_tropa_Inimigo.id = "TrollSafiraInimgo";
+
+let OrfaFloresta_tropa_Inimigo = {...OrfaFloresta_tropa};
+OrfaFloresta_tropa_Inimigo.id ="OrfaFlorestaInimigo";
+
+//Jogador.push(Bherk_tropa, TrollSafira_tropa, OrfaFloresta_tropa);
+//Oponente.push(Bherk_tropa_Inimigo, TrollSafira_tropa_Inimigo, OrfaFloresta_tropa_Inimigo);
--- main.js
+++ main.js
@@ -1,35 +1,82 @@
-var Ataque_A;
-var Defesa_A;
+var carta = null;
+var Mortos = new Array();
+var Jogador= new Array();
+var Oponente = new Array();
+var PVCemiterio = 0;
 
-var Ataque_B;
-var Defesa_B;
 
-var carta = {};
+//var pj = function(a){ Jogador.indexOf(a)};
+//var po = function(a){ Oponente.indexOf(a)};
 
-function Selecionar(nome){
-    
-    carta = nome;
-    console.log(carta);
+function Acao(nome){
+    if(carta == null){
+        Selecionar(nome);
+    }else if(carta.id==nome.id){
+            console.log("Voce não pode atacar a si mesmo! selecione o alvo!");
+    }else if(((Oponente.indexOf(carta) != -1)&&(Oponente.indexOf(nome) != -1)) || ((Jogador.indexOf(carta) != -1)&&(Jogador.indexOf(nome)!= -1))){
+        console.log("Voce não pode atacar um aliado!");
+    }else{
+        Atacar(nome);
+    }
 }
 
-function Atacar(alvo){
-    var dano = carta.ataque - alvo.defesa;
 
-    if(carta.ataque == alvo.defesa){
-        alert('empate');
-    }else if(carta.ataque > alvo.defesa){
-        alvo.pontos_de_vida -= dano;
-    }else if(carta.ataque < alvo.defesa){
-        carta.pontos_de_vida += dano;
-    }
-    console.log(carta.nome + " atacou " + alvo.nome);
-    console.log("PV atacante " + carta.pontos_de_vida);
-    console.log("PV atacado" + alvo.pontos_de_vida);
+function Selecionar(nome){
+    carta = nome
+    console.log(nome);
+}
+
+
+function Atacar(alvo){
+        var dano = carta.ataque - alvo.defesa;
+        if(carta.ataque == alvo.defesa){
+            alert('empate');
+        }else if(carta.ataque > alvo.defesa){
+            alvo.pontos_de_vida -= dano;
+        }else if(carta.ataque < alvo.defesa){
+            carta.pontos_de_vida += dano;
+        }
+        console.log(carta.nome + " atacou " + alvo.nome);
+        console.log("PV atacante " + carta.pontos_de_vida);
+        console.log("PV atacado" + alvo.pontos_de_vida);
+        if(carta.pontos_de_vida < 0){
+            Cemiterio(carta);
+        }
+        if(alvo.pontos_de_vida < 0){
+            Cemiterio(alvo);
+        }
+        carta = null;
 };
 
 
+function Cemiterio(tropa){
+    var img = document.getElementById(tropa.id);
+    img.parentNode.removeChild(img);
+    PVCemiterio += tropa.pvInical;
+    if(PVCemiterio >= 300){
+        console.log("Voce Perdeu");
+    }
+    Mortos.push(tropa);
+}
+
+
+function Selecionar(obj){
+    if(Jogador.indexOf(obj) == -1){
+        Jogador.push(obj);
+    }
+    console.log(Jogador);
+}
+
+
+function Mao(){
+    for(i=0;i<Jogador.length;i++){
+        var link = Jogador[i];
+        document.getElementById("Jogador").innerHTML = "<img id='" + link.id + "' onclick='Acao(" + link + ")' class='cartas' src='Tropas/" + link.nome.replace(/[ ]/g , "_") + ".jpg'>"; 
+    }
+}
+
 window.onload = function inicio(){
-    document.getElementById("Berk").innerHTML = Berk_tropa.nome;
-    document.getElementById("TrollSafira").innerHTML = TrollSafira_tropa.nome;
-    document.getElementById("OrfaFloresta").innerHTML = OrfaFloresta_tropa.nome;
+
+
+
 }
